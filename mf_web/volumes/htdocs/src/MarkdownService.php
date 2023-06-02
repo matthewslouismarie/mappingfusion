2880b9eb141a66389ab3220201f3e28478aa7254
Louis-Marie Matthews <matthewslouismarie@gmail.com>
2023-06-02 14:19:11 +0200

    Improve layout of article editing page

--- mf_web/volumes/htdocs/src/MarkdownService.php
+++ mf_web/volumes/htdocs/src/MarkdownService.php
@@ -6,7 +6,13 @@ use Michelf\Markdown;
 
 class MarkdownService
 {
+    private Markdown $parser;
+
+    public function __construct() {
+        $this->parser = new Markdown();
+        $this->parser->empty_element_suffix = '>';
+    }
     public function format(string $text): string {
-        return Markdown::defaultTransform($text);
+        return $this->parser->transform($text);
     }
 }
