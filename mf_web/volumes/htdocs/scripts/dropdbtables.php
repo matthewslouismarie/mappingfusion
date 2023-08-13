79fcd1dfc628364c59cf9ab2476c92d42901de05
Louis-Marie Matthews <matthewslouismarie@gmail.com>
2023-08-13 22:56:12 +0200

    Add script to drop DB tables (and not the entire database)

---
+++ mf_web/volumes/htdocs/scripts/dropdbtables.php
@@ -0,0 +1,20 @@
+<?php
+
+use MF\Configuration;
+use MF\Database\DatabaseManager;
+
+$container = require_once dirname(__FILE__) . '/../index.php';
+
+$connection = $container->get(DatabaseManager::class);
+$dbName = $container->get(Configuration::class)->getSetting('DB_NAME');
+
+$connection->getPdo()->exec("DROP TABLE e_article;");
+$connection->getPdo()->exec("DROP TABLE e_author;");
+$connection->getPdo()->exec("DROP TABLE e_category;");
+$connection->getPdo()->exec("DROP TABLE e_contribution;");
+$connection->getPdo()->exec("DROP TABLE e_member;");
+$connection->getPdo()->exec("DROP TABLE e_playable_link;");
+$connection->getPdo()->exec("DROP TABLE e_playable;");
+$connection->getPdo()->exec("DROP TABLE e_review;");
+$connection->getPdo()->exec("DROP VIEW v_article;");
+$connection->getPdo()->exec("DROP VIEW v_playable;");
