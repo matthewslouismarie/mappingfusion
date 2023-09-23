ffbad8d7d65a41b1287762ce86932cd9c4be0640
Louis-Marie Matthews <matthewslouismarie@gmail.com>
2023-09-23 15:42:06 +0200

    Put screen dependant stylesheets in separate files to improve performance

---
+++ mf_web/volumes/htdocs/sass.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+vendor/bin/pscss sass/style.scss public/style.css
+vendor/bin/pscss sass/style.size_s.scss public/style.size_s.css
+vendor/bin/pscss sass/style.size_m.scss public/style.size_m.css
+vendor/bin/pscss sass/style.size_l.scss public/style.size_l.css
+vendor/bin/pscss sass/style.size_l2.scss public/style.size_l2.css
+vendor/bin/pscss sass/style.size_xl.scss public/style.size_xl.css
