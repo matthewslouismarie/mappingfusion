2e3b5c3dba1d40e8b14c5e23f307a85a110844f5
Louis-Marie Matthews <matthewslouismarie@gmail.com>
2023-09-24 11:51:52 +0200

    Update progress bar in review

--- mf_web/volumes/htdocs/public/review-menu.js
+++ mf_web/volumes/htdocs/public/review-menu.js
@@ -1,27 +1,29 @@
 class ReviewMenu
 {
     #tocItems;
-    #html;
+    #breakpoints;
 
     constructor() {
         this.#tocItems = document.querySelectorAll('[data-type="toc-item"]');
-        this.#html = document.querySelector('html');
+        this.#breakpoints = new Map();
     }
 
     init() {
         this.#tocItems.forEach((tocItem) => {
             const item = document.querySelector(tocItem.querySelector('a').hash);
-            const itemTop = item.getBoundingClientRect().top + window.scrollY;
-            if (itemTop + window.innerHeight < document.documentElement.scrollHeight) {
-                tocItem.dataset.isReachedY = itemTop;
-            } else {
-                tocItem.dataset.isReachedY = item.getBoundingClientRect().bottom + window.scrollY;
-            }
+            // const itemTop = item.getBoundingClientRect().top + window.scrollY;
+            this.#breakpoints.set(item.id, item.getBoundingClientRect().bottom + window.scrollY);
+            // if (itemTop + window.innerHeight < document.documentElement.scrollHeight) {
+            //     tocItem.dataset.isReachedY = itemTop;
+            // } else {
+            //     tocItem.dataset.isReachedY = item.getBoundingClientRect().bottom + window.scrollY;
+            // }
         });
 
         document.addEventListener('scroll', (ev) => {
             this.#tocItems.forEach((tocItem) => {
-                if (window.scrollY + window.innerHeight > tocItem.dataset.isReachedY) {
+                const breakpoint = this.#breakpoints.get(tocItem.querySelector('a').hash.substring(1));
+                if (window.scrollY + window.innerHeight > breakpoint) {
                     tocItem.classList.add('-active');
                 } else {
                     tocItem.classList.remove('-active');
