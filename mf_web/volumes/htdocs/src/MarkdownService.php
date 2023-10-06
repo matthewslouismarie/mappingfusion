e19f1348ef29deeb9b459340a5e6cdf70c2ef2e9
Louis-Marie Matthews <matthewslouismarie@gmail.com>
2023-10-06 20:12:36 +0200

    Make MF advice customisable

--- mf_web/volumes/htdocs/src/MarkdownService.php
+++ mf_web/volumes/htdocs/src/MarkdownService.php
@@ -6,14 +6,28 @@ use Michelf\Markdown;
 
 class MarkdownService
 {
+    const ADVICE_MARKER = '<!-- Conseil -->';
+
     private Markdown $parser;
 
     public function __construct() {
         $this->parser = new Markdown();
         $this->parser->empty_element_suffix = '>';
     }
+
     public function format(string $text): string {
         return $this->parser->transform($text);
+    }
+
+    /**
+     * @return string[]
+     */
+    public function formatArticleBody(string $text): array {
+        $split = explode(self::ADVICE_MARKER, $text);
+        return [
+            'main' => $this->format($split[0]),
+            'advice' => count($split) > 1 ? $this->format($split[1]) : null,
+        ];
     }
 
     public function getText(string $text): string {
