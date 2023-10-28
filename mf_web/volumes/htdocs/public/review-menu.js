458b27f0214780ed6484aaca31e4b563bf6ce005
Louis-Marie Matthews <matthewslouismarie@gmail.com>
2023-10-28 11:22:38 +0200

    Fix and improve review-menu.js

--- mf_web/volumes/htdocs/public/review-menu.js
+++ mf_web/volumes/htdocs/public/review-menu.js
@@ -4,26 +4,21 @@ class ReviewMenu
     #breakpoints;
 
     constructor() {
-        this.#tocItems = document.querySelectorAll('[data-type="toc-item"]');
-        this.#breakpoints = new Map();
+        this.#tocItems = document.querySelectorAll('[data-type=toc-item]');
+        this.#breakpoints = Object.create(null);
     }
 
     init() {
         this.#tocItems.forEach((tocItem) => {
-            const item = document.querySelector(tocItem.querySelector('a').hash);
-            // const itemTop = item.getBoundingClientRect().top + window.scrollY;
-            this.#breakpoints.set(item.id, item.getBoundingClientRect().bottom + window.scrollY);
-            // if (itemTop + window.innerHeight < document.documentElement.scrollHeight) {
-            //     tocItem.dataset.isReachedY = itemTop;
-            // } else {
-            //     tocItem.dataset.isReachedY = item.getBoundingClientRect().bottom + window.scrollY;
-            // }
+            const hash = tocItem.querySelector('a').hash;
+            const item = document.querySelector(hash);
+            this.#breakpoints[hash] = item.offsetTop;
         });
 
         document.addEventListener('scroll', (ev) => {
             this.#tocItems.forEach((tocItem) => {
-                const breakpoint = this.#breakpoints.get(tocItem.querySelector('a').hash.substring(1));
-                if (window.scrollY + window.innerHeight > breakpoint) {
+                const breakpoint = this.#breakpoints[tocItem.querySelector('a').hash];
+                if (window.scrollY + 0.5 * window.innerHeight > breakpoint) {
                     tocItem.classList.add('-active');
                 } else {
                     tocItem.classList.remove('-active');
