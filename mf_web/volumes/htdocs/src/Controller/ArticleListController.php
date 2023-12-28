ad8715c30f49760e699f1ff7263290399e31a377
Louis-Marie Matthews <17742458+matthewslouismarie@users.noreply.github.com>
2023-12-28 15:33:19 +0100

    Throw exception if category is not found

--- mf_web/volumes/htdocs/src/Controller/ArticleListController.php
+++ mf_web/volumes/htdocs/src/Controller/ArticleListController.php
@@ -4,6 +4,8 @@ namespace MF\Controller;
 
 use GuzzleHttp\Psr7\Response;
 use MF\Enum\Clearance;
+use MF\Exception\Database\EntityNotFoundException;
+use MF\Exception\Http\NotFoundException;
 use MF\Repository\ArticleRepository;
 use MF\Repository\CategoryRepository;
 use MF\TwigService;
@@ -25,31 +27,34 @@ class ArticleListController implements ControllerInterface
         ServerRequestInterface $request,
         array $routeParams,
     ): ResponseInterface {
-        if (key_exists(1, $routeParams)) {
-            $requestedCategoryId = $routeParams[1];
-            $category = $this->categoryRepository->findWithChildren($requestedCategoryId);
-            $i = 0;
-            $categoryIds = ["cat_{$i}" => $category->id];
-            foreach ($category->children as $c) {
-                $categoryIds['cat_' . ++$i] = $c->id;
+        try {
+            if (key_exists(1, $routeParams)) {
+                $requestedCategoryId = $routeParams[1];
+                $category = $this->categoryRepository->findWithChildren($requestedCategoryId);
+                $i = 0;
+                $categoryIds = ["cat_{$i}" => $category->id];
+                foreach ($category->children as $c) {
+                    $categoryIds['cat_' . ++$i] = $c->id;
+                }
+                return new Response(
+                    body: $this->twig->render('article_list.html.twig', [
+                        'articles' => $this->repo->findAllPublished($categoryIds),
+                        'categories' => $category->children,
+                        'category' => $category,
+                    ])
+                );
+            } else {
+                return new Response(
+                    body: $this->twig->render('article_list.html.twig', [
+                        'articles' => $this->repo->findAllPublished(),
+                        'categories' => $this->categoryRepository->findAll(),
+                        'category' => null,
+                    ])
+                );
             }
-            return new Response(
-                body: $this->twig->render('article_list.html.twig', [
-                    'articles' => $this->repo->findAllPublished($categoryIds),
-                    'categories' => $category->children,
-                    'category' => $category,
-                ])
-            );
-        } else {
-            return new Response(
-                body: $this->twig->render('article_list.html.twig', [
-                    'articles' => $this->repo->findAllPublished(),
-                    'categories' => $this->categoryRepository->findAll(),
-                    'category' => null,
-                ])
-            );
+        } catch (EntityNotFoundException $e) {
+            throw new NotFoundException();
         }
-        
     }
 
     public function getAccessControl(): Clearance {
