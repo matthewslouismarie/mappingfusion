54f0e7116efd894f2d5b608df5290bfe98dcf1b3
Louis-Marie Matthews <17742458+matthewslouismarie@users.noreply.github.com>
2024-02-09 19:17:14 +0100

    Add check that category ID is different from its parent category ID

--- mf_web/volumes/htdocs/sql/e_category.sql
+++ mf_web/volumes/htdocs/sql/e_category.sql
@@ -2,5 +2,6 @@ CREATE TABLE IF NOT EXISTS e_category (
     category_id VARCHAR(%1$s) PRIMARY KEY CHECK(category_id REGEXP '%2$s'),
     category_name VARCHAR(%1$s) NOT NULL CHECK(category_name != ''),
     category_parent_id VARCHAR(%1$s),
-    FOREIGN KEY (category_parent_id) REFERENCES e_category (category_id) ON UPDATE CASCADE
+    FOREIGN KEY (category_parent_id) REFERENCES e_category (category_id) ON UPDATE CASCADE,
+    CHECK(category_id <> category_parent_id)
 )
