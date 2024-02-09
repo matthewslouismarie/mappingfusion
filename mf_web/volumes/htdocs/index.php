3a0cc5565aac48a09761587e1cc4cb072795928e
Louis-Marie Matthews <17742458+matthewslouismarie@users.noreply.github.com>
2024-02-09 18:38:16 +0100

    Set language to 'fr' for now

--- mf_web/volumes/htdocs/index.php
+++ mf_web/volumes/htdocs/index.php
@@ -4,4 +4,4 @@ require_once './vendor/autoload.php';
 
 use LM\WebFramework\Kernel;
 
-return Kernel::initialize(realpath(dirname(__FILE__)), 'en');
+return Kernel::initialize(realpath(dirname(__FILE__)), 'fr');
