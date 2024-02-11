798107acacd09a347192c6fb72dc7d6f3bf8b436
Louis-Marie Matthews <17742458+matthewslouismarie@users.noreply.github.com>
2024-02-11 18:22:20 +0100

    Correct category constraint specification on table creation

--- mf_web/volumes/htdocs/sql/e_category.sql
+++ mf_web/volumes/htdocs/sql/e_category.sql
@@ -3,5 +3,5 @@ CREATE TABLE IF NOT EXISTS e_category (
     category_name VARCHAR(%1$s) NOT NULL CHECK(category_name != ''),
     category_parent_id VARCHAR(%1$s),
     FOREIGN KEY (category_parent_id) REFERENCES e_category (category_id) ON UPDATE CASCADE,
-    CHECK(category_id <> category_parent_id)
+    CONSTRAINT UNIQUE(category_id, category_parent_id)
 )
