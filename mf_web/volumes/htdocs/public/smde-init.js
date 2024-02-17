eb41711a85ad2f1b9b3184768c72de1521693c54
Louis-Marie Matthews <17742458+matthewslouismarie@users.noreply.github.com>
2024-02-17 21:27:35 +0100

    Don’t use inline script for SMDE init

---
+++ mf_web/volumes/htdocs/public/smde-init.js
@@ -0,0 +1,33 @@
+const scriptRef = document.getElementById('smde-init');
+const autosaveDateTimeKey = scriptRef.dataset.autosaveDateTimeKey;
+const smdeCacheId = scriptRef.dataset.smdeCacheId;
+const lastUpdateDateTimeUtc = scriptRef.dataset.lastUpdateDateTimeUtc;
+
+if ('' !== lastUpdateDateTimeUtc && null !== localStorage.getItem(autosaveDateTimeKey)) {
+    const lastUpdateDateTime = new Date(lastUpdateDateTimeUtc );
+    const autosaveDateTime = new Date(parseInt(localStorage.getItem(autosaveDateTimeKey)));
+    if (lastUpdateDateTime > autosaveDateTime) {
+        localStorage.removeItem(`smde_${smdeCacheId}`);
+        localStorage.removeItem(autosaveDateTimeKey);
+    }
+}
+
+var simplemde = new SimpleMDE({
+    autosave: {
+        enabled: true,
+        delay: 5000,
+        uniqueId: smdeCacheId,
+    },
+    element: document.getElementById("content"),
+    spellChecker: false,
+});
+
+document.getElementById("content").required = false;
+
+const changes = new UnsavedChanges('submit-button', 'article-form');
+simplemde.codemirror.on('change', function() {
+    changes.init();
+});
+simplemde.codemirror.on("change", function () {
+    localStorage.setItem(autosaveDateTimeKey, Date.now());
+});
