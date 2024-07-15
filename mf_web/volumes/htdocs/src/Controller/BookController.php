d76f3114e199b8f3f41cb0f72af6c66cab05888c
Louis-Marie Matthews <17742458+matthewslouismarie@users.noreply.github.com>
2024-07-15 05:11:28 +0200

    Allow basic display of books

---
+++ mf_web/volumes/htdocs/src/Controller/BookController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace MF\Controller;
+
+use LM\WebFramework\AccessControl\Clearance;
+use LM\WebFramework\Controller\ControllerInterface;
+use LM\WebFramework\DataStructures\AppObject;
+use LM\WebFramework\DataStructures\Page;
+use MF\Repository\BookRepository;
+use MF\TwigService;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
+class BookController implements ControllerInterface
+{
+    public function __construct(
+        private BookRepository $bookRepository,
+        private TwigService $twigService,
+        private PageFactory $pageFactory,
+    ) {
+    }
+
+    public function generateResponse(ServerRequestInterface $request, array $routeParams): ResponseInterface
+    {
+        $book = $this->bookRepository->find($routeParams[1]);
+        return $this->twigService->respond('book.html.twig', $this->getPage($book), ['book' => $book]);
+    }
+
+    public function getAccessControl(): Clearance
+    {
+        return Clearance::ALL;
+    }
+
+    public function getPage(AppObject $book): Page
+    {
+        return $this->pageFactory->create($book['title'], self::class, [$book['id']], HomeController::class);
+    }
+}
