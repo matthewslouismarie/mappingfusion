ebcb9eb7ea806f53727ccb7de279fa20741ad72a
Louis-Marie Matthews <matthewslouismarie@gmail.com>
2025-03-23 02:01:19 +0100

    Make it possible to start kernel with runtime config

--- web_app/index.php
+++ web_app/index.php
@@ -4,4 +4,8 @@ require_once './vendor/autoload.php';
 
 use LM\WebFramework\Kernel;
 
-return Kernel::initialize(realpath(dirname(__FILE__)), 'fr');
+return Kernel::initialize(
+    realpath(dirname(__FILE__)),
+    'fr',
+    isset($runtimeConfig) ? $runtimeConfig : [],
+);
