ae9ec73d9e274452ec1ed1c892356408a74fe8e9
Louis-Marie Matthews <matthewslouismarie@gmail.com>
2025-03-23 03:40:04 +0100

    Fix bad order of joins in v_playable.sql

--- web_app/sql/v_playable.sql
+++ web_app/sql/v_playable.sql
@@ -1,7 +1,7 @@
 CREATE OR REPLACE VIEW v_playable AS SELECT
     p.*,
-    e_author.*,
     e_contribution.*,
+    e_author.*,
     e_playable_link.*,
     game.playable_id AS game_id,
     game.playable_name AS game_name,
@@ -9,8 +9,7 @@ CREATE OR REPLACE VIEW v_playable AS SELECT
     game.playable_type AS game_type,
     game.playable_game_id AS game_game_id
 FROM e_playable AS p
-    LEFT JOIN e_author ON contribution_author_id = author_id
-    LEFT JOIN e_contribution ON p.playable_id = contribution_playable_id
+    LEFT JOIN (e_contribution, e_author) ON p.playable_id = contribution_playable_id AND contribution_author_id = author_id
     LEFT JOIN e_playable_link ON p.playable_id = link_playable_id
     LEFT JOIN e_playable AS game ON p.playable_game_id = game.playable_id
 ;
